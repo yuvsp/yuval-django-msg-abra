0e9559b93566fea8b6f58539317c1f1c31d7c980
Yuval Shapira <yuvalsp@gmail.com>
2022-06-05 22:14:53 +0300

    Update filters.py

--- msgs/filters.py
+++ msgs/filters.py
@@ -2,16 +2,9 @@ from django_filters import rest_framework as filters
 from .models import Msg
 
 
-# We create filters for each field we want to be able to filter on
-# class MovieFilter(filters.FilterSet):
-#     title = filters.CharFilter(lookup_expr='icontains')
-#     genre = filters.CharFilter(lookup_expr='icontains')
-#     year = filters.NumberFilter()
-#     year__gt = filters.NumberFilter(field_name='year', lookup_expr='gt')
-#     year__lt = filters.NumberFilter(field_name='year', lookup_expr='lt')
-#     creator__username = filters.CharFilter(lookup_expr='icontains')
+# class MsgFilter(filters.FilterSet):
 #
 #     class Meta:
-#         model = Movie
-#         fields = ['title', 'genre', 'year', 'year__gt', 'year__lt', 'creator__username']
+#         model = Msg
+#         fields = []
 #
