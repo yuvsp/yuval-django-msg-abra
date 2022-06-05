6516de16d703fd4a30cda3ae41de404c40e44b1c
Yuval Shapira <yuvalsp@gmail.com>
2022-06-05 22:16:07 +0300

    Update models.py

--- msgs/models.py
+++ msgs/models.py
@@ -15,13 +15,3 @@ class Msg(models.Model):
         ordering = ['-id']
 
 
-# class Movie(models.Model):
-#     title = models.CharField(max_length=100)
-#     genre = models.CharField(max_length=100)
-#     year = models.IntegerField()
-#     created_at = models.DateTimeField(auto_now_add=True)
-#     updated_at = models.DateTimeField(auto_now=True)
-#     creator = models.ForeignKey('auth.User', related_name='msgs', on_delete=models.CASCADE)
-#
-#     class Meta:
-#         ordering = ['-id']
